c1788c1d4defd9cd7bf7362704b56237bda9dfa4
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-08-09 18:05:28 +0200

    fix: sorted manual bibliography

--- extract_keys.py
+++ extract_keys.py
@@ -11,8 +11,9 @@ def extract_citation_keys(latex_source):
 
 
 def main():
-    input_file = 'example/example.tex'  # Replace this with the path to your LaTeX source file
-    output_file = 'citation_keys.txt'  # Replace this with the desired output file path
+    input_file = 'input/main.tex'  # Replace this with the path to your LaTeX source file
+    output_file = 'output/citation_keys.txt'  # Replace this with the desired output file path
+    unique_output_file = 'output/unique_citation_keys.txt'  # Replace this with the desired output file path
 
     try:
         with open(input_file, 'r', encoding='utf-8') as file:
@@ -22,15 +23,20 @@ def main():
         return
 
     citation_keys = extract_citation_keys(latex_source)
-
     try:
         with open(output_file, 'w', encoding='utf-8') as output:
             for key in citation_keys:
                 output.write(key + '\n')
+        with open(unique_output_file, 'w', encoding='utf-8') as output:
+            prev_key = ""
+            for key in citation_keys:
+              if prev_key != key:
+                output.write(key + '\n')
+                prev_key = key
     except IOError:
         print(f"Error: Unable to write to '{output_file}'.")
 
-    print(f"Citation keys have been saved to '{output_file}'.")
+    print(f"Citation keys have been saved to '{output_file}' and '{unique_output_file}'.")
 
 if __name__ == "__main__":
     main()
